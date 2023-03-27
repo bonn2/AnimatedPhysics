82961d9d5d43e38d1b235a5c186070380822bad7
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2023-03-27 01:53:54 -0400

    Create Project

---
+++ src/main/java/net/bonn2/animatedphysics/AnimatedPhysics.java
@@ -0,0 +1,17 @@
+package net.bonn2.animatedphysics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnimatedPhysics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
